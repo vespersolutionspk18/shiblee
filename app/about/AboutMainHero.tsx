790acd070f15304cb4243a34deb959673d3cab06
Mac <vespersolutionspk@gmail.com>
2025-01-08 18:17:31 +0500

    first committt

--- app/about/AboutMainHero.tsx
+++ app/about/AboutMainHero.tsx
@@ -9,7 +9,7 @@ const AboutMainHero = () => {
 
 Catering primarily to the students of Sheikhupura and surrounding villages, Shiblee serves as a hub of learning and development.
 <br></br><br></br>
-The average Shiblee students come from a lower or lower-middle class family, yet aspires to achieve excellence and break barriers through their education. The school's comprehensive curriculum and supportive faculty allows students to dream a brighter future.
+The average Shiblee students come from a lower or lower-middle class family, yet aspires to achieve excellence and break barriers through their education. The school&apos;s comprehensive curriculum and supportive faculty allows students to dream a brighter future.
 <br></br><br></br>
 The founding members of Shiblee had a vision, to use education as a tool for social upliftment in Sheikhupura. Recognizing the pivotal role of education in social mobility, they established this institution with the aim of empowering local youth with the confidence to grow into their true potential.
 
